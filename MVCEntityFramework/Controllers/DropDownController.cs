3965b78284f746439cdb3bcdd2a0d859d6b9569f
manish <manish.7151.sharma@gmail.com>
2021-12-28 12:59:50 +0530

    DDl

---
+++ MVCEntityFramework/Controllers/DropDownController.cs
@@ -0,0 +1,46 @@
+﻿using MVCEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEntityFramework.Controllers
+{
+    public class DropDownController : Controller
+    {
+        // GET: DropDown
+        public ActionResult Index()
+        {
+            //ViewBag.CList = new List<string>() { "India" ,"China" , "USA","Japan" };
+            //Employee e = new Employee { Country = "India" };
+
+            ViewBag.CList = new List<Country>() 
+            {
+                new Country {  Id = 1, Text="India"}, 
+                new Country { Id = 2, Text = "China" }
+            };
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+            //ViewBag.CList = new List<string>() { "India", "China", "USA", "Japan" };
+
+            ViewBag.CList = new List<Country>()
+            {
+                new Country {  Id = 1, Text="India"},
+                new Country { Id = 2, Text = "China" }
+            };
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
