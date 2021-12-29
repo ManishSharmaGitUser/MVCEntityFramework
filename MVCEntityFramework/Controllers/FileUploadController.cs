4a2df1c6298588b1d0b7ecfd0ead2b37b5757fe4
manish <manish.7151.sharma@gmail.com>
2021-12-29 22:37:17 +0530

    Download

---
+++ MVCEntityFramework/Controllers/FileUploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using MVCEntityFramework.Models;
+
+namespace MVCEntityFramework.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FileUploadClass model)
+        {
+            if (model.File == null)
+            {
+                ModelState.AddModelError(string.Empty, "No File for Upload");
+                return View();
+            }
+
+            string path = Server.MapPath("~/UploadedFile");
+            string Filename = Path.GetFileName(model.File.FileName);
+
+            string fullpath = Path.Combine(path, Filename);
+            model.File.SaveAs(fullpath);
+            return View();
+        }
+
+        public FileResult Download()
+        {
+            //put this ~/UploadedFile in config file
+            string path = Server.MapPath("~/UploadedFile");
+            string Filename = Path.GetFileName("9.jpg"); // get filename from passing parameter
+
+            string fullpath = Path.Combine(path, Filename);
+            return File(fullpath, "image/jpg");
+        }
+    }
+}
---
+++ MVCEntityFramework/Models/FileUploadClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEntityFramework.Models
+{
+    public class FileUploadClass
+    {
+        public string Name { get; set; }
+
+        public HttpPostedFileBase File { get; set; }
+    }
+}
